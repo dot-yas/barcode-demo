37c51e13c46fc6d99a63e06ac7188e69cb2e3a4a
yas <yasuhiro.kato@d2cid.co.jp>
2023-08-17 14:21:22 +0900

    リンクを表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
-import { useState } from 'react'
-import { Scanner } from './Scanner'
+import { useState } from "react";
+import { Scanner } from "./Scanner";
+import "./App.css";
 
 const App = () => {
-  const [code, setCode] = useState<string>('')
+  const [code, setCode] = useState<string>("");
+
   return (
-    <div>
-      <Scanner
-        onReadCode={(result) => setCode((code) => result.getText())}
-      />
+    <div className="App">
+      <Scanner onReadCode={(result) => setCode(result.getText())} />
       <p>code: {code}</p>
-      <button>ボタン</button>
+      <CodeLink code={code} />
+      <button
+        onClick={(event: any) => {
+          setCode("");
+        }}
+      >
+        リセット
+      </button>
     </div>
-  )
-}
+  );
+};
+
+const CodeLink = (props: { code: String }) => {
+  const host = "https://example.com/01/";
+
+  if (!props.code) {
+    return <></>;
+  } else {
+    return (
+      <div>
+        <a
+          href={`${host}${props.code}`}
+          target="_blank"
+          rel="noreferrer"
+        >{`${host}${props.code}`}</a>
+      </div>
+    );
+  }
+};
 
 export default App;
--- src/Scanner.tsx
+++ src/Scanner.tsx
@@ -1,27 +1,35 @@
-import { BrowserMultiFormatReader } from '@zxing/browser'
-import { Result } from '@zxing/library'
-import { useMemo, useRef } from 'react'
-import { useDebounce } from 'react-use'
+import { BrowserMultiFormatReader } from "@zxing/browser";
+import { Result } from "@zxing/library";
+import { useMemo, useRef } from "react";
+import { useDebounce } from "react-use";
+import "./Scanner.css";
 
 type ScannerProps = {
-  onReadCode?: (text: Result) => void
-}
+  onReadCode?: (text: Result) => void;
+};
 
 export const Scanner = ({ onReadCode }: ScannerProps) => {
-  const videoRef = useRef<HTMLVideoElement>(null)
-  const codeReader = useMemo(() => new BrowserMultiFormatReader(), [])
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const codeReader = useMemo(() => new BrowserMultiFormatReader(), []);
 
   useDebounce(async () => {
-    if (!videoRef.current) return
-    await codeReader.decodeFromVideoDevice(undefined, videoRef.current, (result, error) => {
-      if (!result) return
-      if (error) {
-        console.log('ERROR!! : ', error)
-        return
-      }
-      onReadCode?.(result)
-    })
-  }, 2000)
+    if (!videoRef.current) {
+      return;
+    }
 
-  return <video style={{ width: '100%' }} ref={videoRef} />
-}
+    await codeReader.decodeFromVideoDevice(
+      undefined,
+      videoRef.current ?? undefined,
+      (result, error) => {
+        if (!result) return;
+        if (error) {
+          console.log("ERROR: ", error);
+          return;
+        }
+        onReadCode?.(result);
+      }
+    );
+  }, 1000);
+
+  return <video className="Scanner" ref={videoRef} />;
+};
